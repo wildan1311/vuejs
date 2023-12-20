d6a659d7d5bf7011ef3a7d0f5d0c16e120fb8bfa
wildan1311 <wmaaf1311@gmail.com>
2023-12-20 18:50:39 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mt-5">
+    <h1>{{ title }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum recusandae sed debitis
+      iure in minus possimus aliquam nesciunt ipsam dolor, quia suscipit dolorum ea
+      assumenda laudantium natus quos maxime doloremque.</p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <Price :value="100"></Price>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Price from "./components/Price.vue";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      title: "IDSHOP"
+    }
+  },
+  components: {
+    FontAwesomeIcon,
+    Price,
+  },
+};
+</script>
---
+++ src/components/Price.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span>{{
+      this.prefix + Number.parseFloat(this.value).toFixed(this.precision)
+    }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Number,
+    prefix: {
+      default: "Rp",
+      type: Number
+    },
+    precision: {
+      default: 2,
+      type: Number
+    }
+  }
+};
+</script>
